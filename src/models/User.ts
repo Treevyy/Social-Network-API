1cd0e2a6660e35fa73298e37bed07e72b9da12a5
Treevyy <aarontrevormoore@gmail.com>
2025-04-11 00:56:53 -0500

    added user model an schema

---
+++ src/models/User.ts
@@ -0,0 +1,49 @@
+import { Schema, model } from 'mongoose';
+
+interface IUser extends Document {
+    username: string;
+    email: string;
+    thoughts: Schema.Types.ObjectId[];
+    friends: Schema.Types.ObjectId[];
+}
+
+const userSchema = new Schema<IUser>({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought',
+        },
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+        },
+    ],
+},
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+
+userSchema.virtual('friendCount').get(function (this: IUser) {
+    return this.friends.length;
+});
+
+const User = model('User', userSchema);
+
+export default User;
